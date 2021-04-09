a53282a10d1f81e54e4cf5966a68bd323c0fda78
Pedro Giampietro <pedrogiampietro@hotmail.com>
2021-04-09 11:09:57 -0300

    added template engine, refactoring routes.

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = express.Router();
+
+const views = __dirname + '/views/';
+
+const profile = {
+  name: 'Pedro',
+  avatar: 'https://github.com/pedrogiampietro.png',
+  'monthly-budget': 3000,
+  'hours-per-day': 5,
+  'days-per-week': 5,
+  'vacation-per-year': 4,
+};
+
+routes.get('/', (req, res) => res.render(`${views}index`));
+routes.get('/job', (req, res) => res.render(`${views}job`));
+routes.get('/job/edit', (req, res) => res.render(`${views}job-edit`));
+routes.get('/profile', (req, res) =>
+  res.render(`${views}profile`, { profile })
+);
+
+module.exports = routes;
